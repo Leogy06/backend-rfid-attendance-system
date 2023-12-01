81af6b650a8d1e2385eb05e0b5294a2ec93bad6c
Leogy Santillan <2022_cete_santillanlb@online.htcgsc.edu.ph>
2023-12-02 07:35:26 +0800

    modify imports

--- index.js
+++ index.js
@@ -1,35 +1,20 @@
-import express from "express";
-import { PORT,mongodbURL } from "./config.js";
-import mongoose from "mongoose";
-import { studentRoutes } from "./router/studentRouter.js";
-import  cors  from "cors";
-import { fileURLToPath } from 'url';
-import { dirname, join } from "path";
-import { adminRoutes } from "./router/admin.js";
+import express from "express"
+import { PORT,mongodbURL } from "./config.js"
+import mongoose from "mongoose"
+import { studentRoutes } from "./router/studentRouter.js"
+import  cors  from "cors"
+import { adminRoutes } from "./router/admin.js"
 
+const app = express()
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const app = express();
-
-app.use(express.json());
-// app.use(cors());
+app.use(express.json())
+// app.use(cors())
 
 app.use(cors({
     origin: 'http://127.0.0.1:5500',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
-  }));
-
-app.use(express.static(join(__dirname, '../frontend/node_modules')));
-app.use(express.static(join(__dirname, '../frontend/public2')));
-
-
-app.get('/', (req, res) => {
-    const loginPath = join(__dirname, '../frontend/public2/login.html');
-    res.sendFile(loginPath);
-});
+  }))
 
 app.use('/students', studentRoutes);
 app.use('/admin', adminRoutes);
@@ -40,9 +25,9 @@ mongoose
     .then(() => {
         app.listen(PORT, () => {
             console.log(`Listening on port ${PORT}`)
-        });
-        console.log('Server is connected to database');
+        })
+        console.log('Server is connected to database')
     })
     .catch((error) => {
-        console.log(error);
+        console.log(error)
     })
