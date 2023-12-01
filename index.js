bc250c1a117d4a514d5b05b892d16ad7aa9b5a81
Leogy Santillan <2022_cete_santillanlb@online.htcgsc.edu.ph>
2023-12-01 15:36:09 +0800

    adding the login page

--- index.js
+++ index.js
@@ -3,23 +3,32 @@ import { PORT,mongodbURL } from "./config.js";
 import mongoose from "mongoose";
 import { studentRoutes } from "./router/studentRouter.js";
 import  cors  from "cors";
+import { fileURLToPath } from 'url';
+import { dirname, join } from "path";
+
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
 app.use(cors({
     origin: 'http://127.0.0.1:5500',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   }));
-  
+
+app.use(express.static(join(__dirname, '../frontend/node_modules')));
+app.use(express.static(join(__dirname, '../frontend/public2')));
+
 
 app.get('/', (req, res) => {
-    console.log(req);
-    return res.status(200).send('Hello!')
-})
+    const loginPath = join(__dirname, '../frontend/public2/login.html');
+    res.sendFile(loginPath);
+});
 
 app.use('/students', studentRoutes);
 
