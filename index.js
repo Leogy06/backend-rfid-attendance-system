6ae1ceef07f04b82ab551ae72e86db0a92121db8
Leogy Santillan <2022_cete_santillanlb@online.htcgsc.edu.ph>
2023-12-09 07:49:21 +0800

    arrange structure of imports

--- index.js
+++ index.js
@@ -1,35 +1,45 @@
-import express from "express"
-import { PORT,mongodbURL } from "./config.js"
-import mongoose from "mongoose"
-import { studentRoutes } from "./router/studentRouter.js"
-import { adminRoutes } from "./router/admin.js"
+import express from "express";
+import { PORT, mongodbURL } from "./config.js";
+import mongoose from "mongoose";
+import { studentRoutes } from "./router/studentRouter.js";
+import { adminRoutes } from "./router/admin.js";
+import passport from "passport";
+import session from "express-session";
+import flash from "express-flash";
+import { mySecretKey } from "./config.js";
 
-const app = express()
+const app = express();
 
-app.use(express.json())
-
-app.set('view-engine', 'ejs')
-
-app.use(express.static('public'))
-
-app.use(express.urlencoded({ extended:false}))
-
-app.get('/', (req, res) => {
-    res.render('../src/views/home.ejs', {name: 'Leogy'})
-})
-
-app.use('/students', studentRoutes)
-app.use('/admin', adminRoutes)
+app.use(express.json());
 
 mongoose
-    .connect(mongodbURL)
+  .connect(mongodbURL)
 
-    .then(() => {
-        app.listen(PORT, () => {
-            console.log(`Listening on port ${PORT}`)
-        })
-        console.log('Server is connected to database')
-    })
-    .catch((error) => {
-        console.log(error)
-    })
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Listening on port ${PORT}`);
+    });
+    console.log("Server is connected to database");
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
+app.set("view-engine", "ejs");
+
+app.use(express.static("public"));
+
+app.use(express.urlencoded({ extended: false }));
+app.use(passport.initialize());
+app.use(
+  session({
+    secret: mySecretKey(),
+    resave: false,
+    saveUninitialized: false,
+  })
+);
+app.use(flash());
+app.use(passport.session());
+
+app.use("/students", studentRoutes);
+app.use("/admin", adminRoutes);
