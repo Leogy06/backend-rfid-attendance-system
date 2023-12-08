42afbeb7fe05bce173fa3087583791c72ce629cc
Leogy Santillan <2022_cete_santillanlb@online.htcgsc.edu.ph>
2023-12-09 07:50:05 +0800

    added passport-config module

---
+++ passport-config.js
@@ -0,0 +1,48 @@
+import { Strategy as LocalStrategy } from "passport-local";
+import bcrypt from "bcrypt";
+import { Admins } from "./models/admin.js";
+import passport from "passport";
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const admin = await Admins.findOne({ email });
+
+        if (!admin) {
+          return done(null, false, { message: "Email does not found." });
+        }
+
+        const correctPass = await bcrypt.compare(password, admin.password);
+
+        if (!correctPass) {
+          return done(null, false, { message: "Incorrect password." });
+        }
+
+        return done(null, admin);
+      } catch (error) {
+        console.error(error);
+        return done(error, false, {
+          message: "Can't authenticate at the moment.",
+        });
+      }
+    }
+  )
+);
+
+passport.serializeUser((admin, done) => {
+  done(null, admin._id);
+});
+
+passport.deserializeUser(async (id, done) => {
+  try {
+    const admin = await Admins.findById(id);
+    done(null, admin);
+  } catch (error) {
+    console.error(error);
+    done(error);
+  }
+});
+
+export default passport;
